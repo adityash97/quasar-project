cae836c599c76e224a749be7114abaf65b8b5ddb
adityash97 <adityash212@gmail.com>
2022-12-25 13:50:14 +0530

    status color readded

--- src/components/projectplanner/ProjectDisplayCard.vue
+++ src/components/projectplanner/ProjectDisplayCard.vue
@@ -1,58 +1,67 @@
 <template>
   <q-card>
-    <q-card-section horizontal>
-      <q-card-section
-        class="justify-around"
-        :class="project.completed ? 'bg-positive' : 'bg-warning'"
-      >
-      </q-card-section>
-      <q-card-section>
-        <div class="row">
-          <div class="col-9">
-            <div class="row">
-              <div class="text-h6">{{ project.title }}</div>
-            </div>
-            <div class="row">
-              <div class="text-body1">{{ project.details }}</div>
-            </div>
-          </div>
-          <div class="col-3 text-right q-gutter-sm">
-            <!-- completed or not -->
-            <div class="row">
-              <div class="col">
-                <q-btn round>
-                  <q-avatar
-                    icon="check"
-                    :text-color="project.completed ? 'positive' : ''"
-                    @click="$emit('toggleCompleted', project)"
-                  />
-                </q-btn>
-              </div>
-            </div>
+    <!--
 
-            <!-- edit -->
-            <div class="row">
-              <div class="col">
-                <q-btn round @click="$emit('showProjectForm', project, true)">
-                  <q-avatar icon="edit" text-color="warning" />
-                </q-btn>
+      <q-card-section
+        class=""
+
+      >
+      </q-card-section> -->
+
+    <q-card-section>
+      <div class="row q-gutter-sm">
+        <div
+          class="col-1 justify-around"
+          :class="project.completed ? 'bg-positive' : 'bg-warning'"
+        ></div>
+        <div class="col">
+          <div class="row">
+            <div class="col-lg-9">
+              <div class="row">
+                <div class="text-h6">{{ project.title }}</div>
+              </div>
+              <div class="row">
+                <div class="text-body1">{{ project.details }}</div>
               </div>
             </div>
-            <!-- delete -->
-            <div class="row">
-              <div class="col">
-                <q-btn round>
-                  <q-avatar
-                    icon="delete"
-                    text-color="negative"
-                    @click="$emit('onDelete', project)"
-                  />
-                </q-btn>
+            <div class="col-3 text-right q-gutter-sm">
+              <!-- completed or not -->
+              <div class="row">
+                <div class="col">
+                  <q-btn round>
+                    <q-avatar
+                      icon="check"
+                      :text-color="project.completed ? 'positive' : ''"
+                      @click="$emit('toggleCompleted', project)"
+                    />
+                  </q-btn>
+                </div>
+              </div>
+
+              <!-- edit -->
+              <div class="row">
+                <div class="col">
+                  <q-btn round @click="$emit('showProjectForm', project, true)">
+                    <q-avatar icon="edit" text-color="warning" />
+                  </q-btn>
+                </div>
+              </div>
+              <!-- delete -->
+              <div class="row">
+                <div class="col">
+                  <q-btn round>
+                    <q-avatar
+                      icon="delete"
+                      text-color="negative"
+                      @click="$emit('onDelete', project)"
+                    />
+                  </q-btn>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </q-card-section>
+      </div>
     </q-card-section>
   </q-card>
 </template>
